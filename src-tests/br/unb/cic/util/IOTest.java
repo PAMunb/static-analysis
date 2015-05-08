92acb9e33f4a75559f0b376ac98029d9849b82d6
Thiago Cavalcanti <cavalcanti.thiago@gmail.com>
2015-05-08 11:04:26 -0300

    branch for tests

---
+++ src-tests/br/unb/cic/util/IOTest.java
@@ -0,0 +1,50 @@
+package br.unb.cic.util;
+
+import java.io.IOException;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import br.unb.cic.sa.util.IO;
+
+public class IOTest {
+
+	@Test
+	public void testListFiles(){
+		try {
+			List<String> files = IO.listFiles("/home/thiago/git/static-analysis/src/br/unb/cic/sa/util", new String[]{"java"});
+			Assert.assertEquals(4, files.size());
+			
+			Assert.assertTrue(files.contains("/home/thiago/git/static-analysis/src/br/unb/cic/sa/util/Constants.java"));
+			Assert.assertTrue(files.contains("/home/thiago/git/static-analysis/src/br/unb/cic/sa/util/IO.java"));
+			Assert.assertTrue(files.contains("/home/thiago/git/static-analysis/src/br/unb/cic/sa/util/ReadCsv.java"));
+			Assert.assertTrue(files.contains("/home/thiago/git/static-analysis/src/br/unb/cic/sa/util/WriteCsv.java"));
+			
+		} catch (IOException e) {
+			Assert.fail(e.getMessage());
+		}
+	}
+	
+	
+	
+	@Test
+	public void testListFilesWithoutFilters() {
+		try {
+			List<String> files = IO.listFiles("/home/thiago/git/static-analysis/src/br/unb/cic/sa/util", new String[]{});
+			
+			Assert.assertEquals(4, files.size());
+			
+			Assert.assertTrue(files.contains("/home/thiago/git/static-analysis/src/br/unb/cic/sa/util/Constants.java"));
+			Assert.assertTrue(files.contains("/home/thiago/git/static-analysis/src/br/unb/cic/sa/util/IO.java"));
+			Assert.assertTrue(files.contains("/home/thiago/git/static-analysis/src/br/unb/cic/sa/util/ReadCsv.java"));
+			Assert.assertTrue(files.contains("/home/thiago/git/static-analysis/src/br/unb/cic/sa/util/WriteCsv.java"));
+			
+		} catch (IOException e) {
+			Assert.fail(e.getMessage());
+		}
+	}
+	
+	
+}
