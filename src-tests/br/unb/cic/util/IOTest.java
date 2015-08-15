114d64ebf845a3c518879bf73fa7452e83f156e8
Thiago Cavalcanti <cavalcanti.thiago@gmail.com>
2015-08-14 22:00:15 -0300

    solve conflicts

--- src-tests/br/unb/cic/util/IOTest.java
+++ src-tests/br/unb/cic/util/IOTest.java
@@ -3,12 +3,7 @@ package br.unb.cic.util;
 import java.io.IOException;
 import java.util.List;
 
-<<<<<<< HEAD
 import org.junit.Assert;
-=======
-import junit.framework.Assert;
-
->>>>>>> 795140d39878b41b6399d5c6ab3c3c3ab9fcf937
 import org.junit.Test;
 
 import br.unb.cic.sa.util.IO;
@@ -17,15 +12,7 @@ public class IOTest {
 
 	@Test
 	public void testListFiles(){
-		try {
-			List<String> files = IO.listFiles("src/br/unb/cic/sa/util", new String[]{"java"});
-			Assert.assertEquals(5, files.size());
-//			
-//			Assert.assertTrue(files.contains("src/br/unb/cic/sa/util/Constants.java"));
-//			Assert.assertTrue(files.contains("src/br/unb/cic/sa/util/IO.java"));
-//			Assert.assertTrue(files.contains("src/br/unb/cic/sa/util/ReadCsv.java"));
-//			Assert.assertTrue(files.contains("src/br/unb/cic/sa/util/WriteCsv.java"));
-//			
+		try {	
 			List<String> files = IO.listFiles("/home/thiago/git/static-analysis/src/br/unb/cic/sa/util", new String[]{"java"});
 			Assert.assertEquals(4, files.size());
 			
@@ -44,15 +31,7 @@ public class IOTest {
 	@Test
 	public void testListFilesWithoutFilters() {
 		try {
-			List<String> files = IO.listFiles("src/br/unb/cic/sa/util", new String[]{});
-			
-			Assert.assertEquals(5, files.size());
-			
-//			Assert.assertTrue(files.contains("/home/thiago/git/static-analysis/src/br/unb/cic/sa/util/Constants.java"));
-//			Assert.assertTrue(files.contains("/home/thiago/git/static-analysis/src/br/unb/cic/sa/util/IO.java"));
-//			Assert.assertTrue(files.contains("/home/thiago/git/static-analysis/src/br/unb/cic/sa/util/ReadCsv.java"));
-//			Assert.assertTrue(files.contains("/home/thiago/git/static-analysis/src/br/unb/cic/sa/util/WriteCsv.java"));
-//			
+
 			List<String> files = IO.listFiles("/home/thiago/git/static-analysis/src/br/unb/cic/sa/util", new String[]{});
 			
 			Assert.assertEquals(4, files.size());
